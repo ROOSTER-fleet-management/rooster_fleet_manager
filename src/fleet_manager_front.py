3bf61b9d57a552f94ec137e8ec7c50ae0fcd06b3
patrick.keesmaat <pkeesmaat@tudelft.nl>
2020-07-24 15:16:38 +0200

    Added visual GUI.

--- src/fleet_manager_front.py
+++ src/fleet_manager_front.py
@@ -1,15 +1,74 @@
 #! /usr/bin/env python
 import json         # Used for reading JSON files (loading jobs to JobQueue)
 import os           # Used to get base filename and file and directory handling
+import sys
 
 import rospy
+from PyQt4 import QtGui, QtCore
+
 from simple_sim.srv import PlaceOrder, PlaceOrderRequest
+from ui import fleet_manager_ui
 
 #region ################################### TO DO LIST #####################################
 # DONE 1. Perform simple call /job_manager/place_order service.
 # DONE 2. Load list of orders from JSON.
 # DONE 3. Call /job_manager/place_order(s?) service with the list of orders.
+# DONE 4. Add visually correct but non-functional GUI to node.
+# TODO 5. Replace JSON order list call and instead visualize it in the Order tab.
+# TODO 6. Connect MEx Sentinel to Mobile Executors treeWidget view.
 #endregion #################################################################################
+
+
+VERSION = "1.0"
+APPLICATION_TITLE = "Fleet Manager"
+print(APPLICATION_TITLE + ". Version: "+VERSION)
+
+
+#region         ### PyQt GUI ###
+class GuiMainWindow(fleet_manager_ui.Ui_MainWindow, QtGui.QMainWindow):
+    def __init__(self):
+        """
+        Initialise the ui widgets, items and varibles.
+        Connect up all UI interactions to their methods. Define status tips.
+        """
+        super(GuiMainWindow, self).__init__()
+        self.setWindowTitle(APPLICATION_TITLE)  #self.filename + " - " + 
+
+        # Set up gui
+        self.setupUi(self)
+    
+    def close_application(self):
+        """Prompts the user if they are sure they which to quit the application before quitting."""
+        choice = QtGui.QMessageBox.question(self, 
+                                            'Quit application?',
+                                            "Are you sure you want to quit? Any unsaved changed will be lost!", 
+                                            QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
+        
+        if choice == QtGui.QMessageBox.Yes:
+            print("Closing Fleet Manager node...")
+            QtCore.QCoreApplication.instance().quit()
+        else:
+            pass
+    
+    def closeEvent(self, event):
+        """Takes control of the close event, making sure the user cannot close the application before prompting them."""
+        event.ignore()
+        self.close_application()
+    
+    def about(self):
+        """Display a MessageBox with the application title, version number and general information."""
+        text = "<center>" \
+            "<h2>"+APPLICATION_TITLE+"</h2>" \
+            "</center>" \
+            "The ROS package simple_sim is created by the Human " \
+            "Robot Coproduction research group at the Industrial Design " \
+            "Engineering faculty of the Delft University of Technology." \
+            "<p>Version: "+VERSION+"<br/>" \
+            "License: Apache License version 2.0</p>"
+        QtGui.QMessageBox.about(self, "About - " + APPLICATION_TITLE + ".", text)
+
+#endregion      ### PyQt GUI ###
+
 
 def add_orders_from_dict(order_dict):
     """ Add multile orders to the order_list from a dictionary. """
@@ -37,7 +96,7 @@ def load_orders_from_JSON(filepath):
         loaddata_dict = json.load(json_loadfile)
     
     # Add orders from loaded JSON dictionary to the order_list.
-    add_orders_from_dict(loaddata_dict)
+    # add_orders_from_dict(loaddata_dict)
 
 if __name__ == '__main__':
     try:     
@@ -84,7 +143,17 @@ if __name__ == '__main__':
             pass
         load_orders_from_JSON(filename)
 
+        #region --- GUI ---
+        app = QtGui.QApplication(sys.argv)
+        appGui = GuiMainWindow()
+        #windowIcon = QtGui.QIcon()
+        #windowIcon.addPixmap(QtGui.QPixmap(":/icons/Launch Icon Multi.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        #appGui.setWindowIcon(windowIcon)
+        appGui.show()
+        app.exec_()
+        #endregion
+
         # Keep node running.
-        rospy.spin()
+        # rospy.spin()
     except rospy.ROSInterruptException:
         pass
