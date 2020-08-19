307dc6114c26bc1390b8e4ee3d18b34484f71c05
neel-stark <40642577+neel-stark@users.noreply.github.com>
2020-08-19 11:14:48 +0200

    using ros-theme for documentation

--- doc/conf.py
+++ doc/conf.py
@@ -29,6 +29,11 @@ catkin_package = catkin_pkg.package.parse_package(os.path.join(catkin_dir, catki
 # setting html_logo to ros logo
 html_logo = 'ros.png'
 
+# Adding catkin-sphinx themes to html_theme_path
+import catkin_sphinx
+html_theme_path = [os.path.join(os.path.dirname(catkin_sphinx.__file__), 'theme')]
+
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -99,7 +104,9 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'ros-theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
